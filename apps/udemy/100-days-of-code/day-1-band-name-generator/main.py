7e866304e3144dc2b712f4db25d48ca4b74b828a
eGaruru <162013145+eGaruru@users.noreply.github.com>
2026-08-04 18:39:46 +0200

    feat(band-name-generator): add project file

---
+++ apps/udemy/100-days-of-code/day-1-band-name-generator/main.py
@@ -0,0 +1,14 @@
+# BAND NAME GENERATOR
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+print(city_name)
+pet_name = input("What's your pet's name?\n")
+print(pet_name)
+
+print("Your band name could be " + city_name + " " + pet_name)
+
+# SOLUTION
+# print("Welcome to the Band Name Generator.")
+# city = input("What's the name of the city you grew up in?\n")
+# pet = input("What's your pet's name?\n")
+# print("Your band name could be " + city + " " + pet)
