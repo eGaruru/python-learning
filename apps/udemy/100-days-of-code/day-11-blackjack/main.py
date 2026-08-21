1422946d9c4e2e75c86f397b97d816dbc2cabe45
eGaruru <162013145+eGaruru@users.noreply.github.com>
2026-08-21 09:59:58 +0200

    [WIP] feat(blackjack): extract the winner-checking function and add an explanation

--- apps/udemy/100-days-of-code/day-11-blackjack/main.py
+++ apps/udemy/100-days-of-code/day-11-blackjack/main.py
@@ -6,26 +6,42 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
 def draw_card():
+    """Draw a random card."""
     return random.choice(cards)
 
 
 def calculate_score(hand):
+    """Calculate the score of a hand and return the score.
+    When user's score went over 21 and has 11, replace 11 to 1."""
     if sum(hand) > 21 and 11 in hand:
         hand[hand.index(11)] = 1
 
     return sum(hand)
 
+def check_winner(user_score, dealer_score):
+    """Compare user score and dealer score to check winner."""
+    if user_score > 21:
+        print("You went over. You lose😭")
+    elif dealer_score > 21:
+        print("Dealer went over. You win!🥳")
+    elif user_score == dealer_score:
+        print("Draw😲")
+    elif user_score > dealer_score:
+        print("You win!🥳")
+    else:
+        print("You lose😭")
 
 def play_game():
     print(logo)
 
     user_cards = [draw_card(), draw_card()]
     dealer_cards = [draw_card(), draw_card()]
+    user_score = calculate_score(user_cards)
+    dealer_score = calculate_score(dealer_cards)
 
     is_game_over = False
-    while is_game_over:
+    while not is_game_over:
         user_score = calculate_score(user_cards)
-        dealer_score = calculate_score(dealer_cards)
 
         print(f"Your cards: {user_cards}, current score: {user_score}")
         print(f"Dealer's first card: {dealer_cards[0]}")
@@ -37,7 +53,6 @@ def play_game():
 
             if user_score > 21:
                 is_game_over = True
-                print("You went over. You lose😭")
         else:
             is_game_over = True
 
@@ -47,12 +62,7 @@ def play_game():
 
     print(f"Your final hand: {user_cards}, final score {user_score}")
     print(f"Dealer's final hand: {dealer_cards}, final score {dealer_score}")
-    if user_score == dealer_score:
-        print("Draw")
-    elif user_score > dealer_score:
-        print("You win!🥳")
-    else:
-        print("You lose😭")
+    check_winner(user_score, dealer_score)
 
 
 while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
